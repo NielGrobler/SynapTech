059c775ec1fd199d3a5a935dfb49e23ea2944dbd
justinz-songz <2445736@students.wits.ac.za>
2025-04-08 21:25:23 +0200

    First commit containing sign up page with passes unit test using jest

---
+++ googleSignUp.js
@@ -0,0 +1,19 @@
+function handleCredentialResponse(response) {
+    const payload = parseJwt(response.credential);
+    window.location.href = "signedUp.html";
+    return payload;
+}  
+
+
+function parseJwt(token) {
+    const base64Url = token.split('.')[1]; 
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+    return JSON.parse(jsonPayload);
+  }
+
+
+  module.exports = { handleCredentialResponse };
+  
