8017645efaa8af2e318dfb30fe9eae8e78dbb79d
Pabi Moloantoa <paballomoloantoa3858@gmail.com>
2025-04-12 17:56:42 +0200

    fixed login with orcid file

--- loginORCID.js
+++ loginORCID.js
@@ -1,61 +1,26 @@
-const express = require('express');
-const axios = require('axios');
-const app = express();
-const port = 3000;
+window.onload = () => {
+  const orcidLoginButton = document.getElementById("orcidLoginButton");
 
-const CLIENT_ID = 'APP-3I8F4OWN73DJB46C';  
-const CLIENT_SECRET = '007072fe-49dd-4410-ac42-bf7e71135c1b';  
-const REDIRECT_URI = 'http:127.0.0.1:550/signedUp.html';
+  const ORCID_CLIENT_ID = "APP-3I8F4OWN73DJB46C";
+  const REDIRECT_URI = "http://127.0.0.1:5500/SynapTech/frontend/signedUp.html";
+  const authURL = `https://orcid.org/oauth/authorize?client_id=${ORCID_CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
 
-app.use(express.static('public'));
-
-// 1. Redirect user to ORCID login
-app.get('/login', (req, res) => {
-  const authUrl = `https://orcid.org/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&scope=/authenticate&redirect_uri=${REDIRECT_URI}`;
-  res.redirect(authUrl);
-});
-
-// 2. ORCID redirects here after login
-app.get('/callback', async (req, res) => {
-  const code = req.query.code;
-
-  try {
-    const response = await axios.post('https://orcid.org/oauth/token', null, {
-      params: {
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: REDIRECT_URI
-      },
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
-
-    const accessToken = response.data.access_token;
-    const orcid = response.data.orcid;
-
-    res.send(`Logged in as ORCID iD: ${orcid}`);
-  } catch (error) {
-    console.error(error.response.data);
-    res.send('Error during OAuth');
+  if (orcidLoginButton) {
+      orcidLoginButton.href = authURL;
   }
-});
+};
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+//Function to get info from the Orcid Response, modify it as you want, use what yoy get here to write jest tests
+function handleOrcidResponse(response) {
+  const params = new URLSearchParams(response);
+  const name = params.get("name");
+  const email = params.get("email");
 
+  const payload = { name, email };
 
+  window.location.href = "signedUp.html";
 
+  return payload;
+}
 
-
-
-
-
-
-
-
-
-
+module.exports = { handleOrcidResponse };
