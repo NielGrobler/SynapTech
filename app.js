824c8841a41c72915b52edf183c1daf5310b1ff8
ell <2710345@students.wits.ac.za>
2025-04-27 08:12:20 +0200

    Create app.js to match ellie2

---
+++ app.js
@@ -0,0 +1,14 @@
+import https from 'https';
+import fs from 'fs';
+import router from './router.js';
+
+const sslOptions = {
+	key: fs.readFileSync('server.key'),
+	cert: fs.readFileSync('server.cert')
+};
+
+const port = process.env.PORT || 3000;
+
+https.createServer(sslOptions, router).listen(port, () => {
+	console.log(`HTTPS server running at https://localhost:${port}`);
+});
