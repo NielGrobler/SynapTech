1c1c60438af5499d17cd7bb8531a1d6c29ac4d4f
NielGrobler <2690034@students.wits.ac.za>
2025-04-28 22:34:08 +0200

    removed coverage folder and excluded it from git (for now, can change) and removed unneeded files

--- vitest.config.js
+++ vitest.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		coverage: {
-			reporter: ['text', 'html'], // show in terminal + generate HTML report
+			reporter: ['text', 'html'],
 			exclude: ['**/node_modules/**', '**/tests/**', '**/public/**']
 		},
 	},
