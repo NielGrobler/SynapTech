0fbc2355621eb248b859a5dbc8b4653bff765f09
NielGrobler <2690034@students.wits.ac.za>
2025-05-14 23:40:25 +0200

    app.js set up to now deploy the server differently when NODE_ENV=production (which is set in the yml for deployment)

--- app.js
+++ app.js
@@ -2,14 +2,19 @@ import https from 'https';
 import fs from 'fs';
 import router from './router.js';
 
-const sslOptions = {
-	key: fs.readFileSync('server.key'),
-	cert: fs.readFileSync('server.cert')
-};
-
 const port = process.env.PORT || 3000;
 
-https.createServer(sslOptions, router).listen(port, () => {
-	console.log(`HTTPS server running at https://localhost:${port}`);
-});
+if (process.env.NODE_ENV === 'production') {
+	router.listen(port, () => {
+		console.log(`HTTPS server running in production mode`);
+	});
+} else { //only other option is running locally if it's not set, or rather when it is unspecified.
+	const sslOptions = {
+		key: fs.readFileSync('server.key'),
+		cert: fs.readFileSync('server.cert')
+	};
 
+	https.createServer(sslOptions, router).listen(port, () => {
+		console.log(`HTTPS server running locally at https://localhost:${port}`);
+	});
+}
