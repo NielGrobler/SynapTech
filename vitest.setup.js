b31d207390901718c7bb33cbbae45612b5ef92cf
NielGrobler <2690034@students.wits.ac.za>
2025-05-19 00:38:24 +0200

    added orcid mocks

--- vitest.setup.js
+++ vitest.setup.js
@@ -5,3 +5,7 @@ process.env.DB_API_KEY = 'testkey';
 process.env.GOOGLE_CLIENT_ID = 'test';
 process.env.GOOGLE_CLIENT_SECRET = 'test';
 process.env.GOOGLE_CALLBACK_URL = 'http://localhost/callback';
+
+process.env.ORCID_CLIENT_ID = 'test';
+process.env.ORCID_CLIENT_SECRET = 'test';
+process.env.ORCID_CALLBACK_URL = 'http://localhost/callback';
