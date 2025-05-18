abd7ca30421a451db4102d4e680380b7d98e72ab
NielGrobler <2690034@students.wits.ac.za>
2025-05-19 00:34:48 +0200

    added mocked values for google auth

--- vitest.setup.js
+++ vitest.setup.js
@@ -1,3 +1,7 @@
 process.env.DB_HOST = 'testhost';
 process.env.DB_PORT = '1234';
 process.env.DB_API_KEY = 'testkey';
+
+process.env.GOOGLE_CLIENT_ID = 'test';
+process.env.GOOGLE_CLIENT_SECRET = 'test';
+process.env.GOOGLE_CALLBACK_URL = 'http://localhost/callback';
