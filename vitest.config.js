9c0361921bdf4acfa860e4884c6847898a252453
NielGrobler <2690034@students.wits.ac.za>
2025-05-22 06:38:59 +0200

    vitest is now more verbose to list all the test cases

--- vitest.config.js
+++ vitest.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		reporters: ['verbose'],
 		environment: 'jsdom', 
 		setupFiles: './vitest.setup.js',
 		coverage: {
